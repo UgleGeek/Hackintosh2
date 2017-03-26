5b76c64c2768cbf387eea759299df7e8648a8544
Erik Steen Sørensen <ubergeek@uglenet.dk>
2017-03-26 12:06:44 +0200

    Initial Commit

---
+++ hackintosh2/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  hackintosh2
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project hackintosh2
+  Created by Erik Steen Sørensen on 26/03/17
+  Copyright © 2017 Erik Steen Sørensen
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ hackintosh2/hackintosh2.ino
@@ -0,0 +1,64 @@
+//
+// hackintosh2
+//
+// Description of the project
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		Erik Steen Sørensen
+// 				Erik Steen Sørensen
+//
+// Date			26/03/17 12:06
+// Version		<#version#>
+//
+// Copyright	© Erik Steen Sørensen, 2017
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
